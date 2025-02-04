ed178d69385fefc1eeba501dc21995e642211df3
William Flageol <zhevilh@users.noreply.github.com>
2025-02-04 12:29:21 -0500

    Ajout code du cours 4 et chapitre 4

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 - [Chapitre 1](https://github.com/wflageol-uqtr/PIF6005/blob/main/chapitre1.md)
 - [Chapitre 2](https://github.com/wflageol-uqtr/PIF6005/blob/main/chapitre2.md)
 - [Chapitre 3](https://github.com/wflageol-uqtr/PIF6005/blob/main/chapitre3.md)
-- Chapitre 4
+- [Chapitre 4](https://github.com/wflageol-uqtr/PIF6005/blob/main/chapitre4.md)
 
 ## Travaux
 - [Travail 1](https://classroom.github.com/a/weZrCBqM) (Date de remise : 27 janvier 2025)
---
+++ automate.fs
@@ -0,0 +1,56 @@
+﻿open System
+
+type IndexedString = { string: string; mutable index : int }
+
+let peekChar { string = string; index = index } = 
+    if index < string.Length
+    then Some string.[index]
+    else None
+
+let incIndex istring =
+    istring.index <- istring.index + 1
+        
+let popChar istring =
+    let c = peekChar istring
+    incIndex istring
+    c
+
+let skipWhitespace istring =
+    while peekChar istring = Some ' ' do incIndex istring
+
+
+let nextChar charTable istring =
+    match peekChar istring with
+    | None -> false
+    | Some c -> if List.contains c charTable
+                then incIndex istring
+                     true
+                else false
+
+let chiffre = nextChar ['0'..'9']
+let nonzero = nextChar ['1'..'9']
+
+let entier istring =
+    let decimalPart () =
+        incIndex istring
+        match peekChar istring with
+        | None -> true
+        | Some c -> while chiffre istring do ()
+                    match peekChar istring with
+                    | None -> true
+                    | _ -> false
+
+    match peekChar istring with
+    | None -> false
+    | Some c -> if c = '0'
+                then incIndex istring
+                     true
+                elif c = '-'
+                then decimalPart ()
+                elif nonzero istring
+                then decimalPart ()
+                else false
+
+[<EntryPoint>]
+let main args =
+    0
